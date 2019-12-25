be961b17e037b3a4fcda96d59cdafc283e1ade0e
Xenon4416 <xenonshrestha4416@gmail.com>
2019-12-25 10:02:39 +0545

    added routing for multipage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component, Fragment } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import SignIn from './SignIn';
 import './App.css';
+
 
 class App extends Component {
     state = {
         displayMode: 'SignIn',
-        signin: { email: null, password: null }
+        signin: { email: null, password: null },
+        isAuth: false
     };
 
     emailChangeHandler = (event) => {
         let temp = {...this.state.signin};
         temp.email = event.target.value;
+        console.log('asd');
         this.setState({signin: temp})
     };
 
@@ -34,8 +38,14 @@ class App extends Component {
     };
 
     render(){
-        let mode = null;
-        if (this.state.displayMode === 'SignIn'){
+        let mode = <Switch>
+            <Route path='/' exact render={() => <SignIn
+                email={this.emailChangeHandler}
+                password={this.passwordChangeHandler}
+                signin={this.onSignInHandler}/>}/>
+            <Redirect to='/'/>
+        </Switch>;
+        /*if (this.state.displayMode === 'SignIn'){
             mode = <SignIn
                 email={this.emailChangeHandler}
                 password={this.passwordChangeHandler}
@@ -43,7 +53,7 @@ class App extends Component {
         }
         else if (this.state.displayMode === 'Admin'){ mode = null }
         else if (this.state.displayMode === 'Teacher'){ mode = null }
-        else if (this.state.displayMode === 'Student'){ mode = null }
+        else if (this.state.displayMode === 'Student'){ mode = null }*/
         return (
             <Fragment>
                 { mode }
