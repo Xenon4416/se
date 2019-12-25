fa32a355b266bb3253891e55f13980b73fe5e917
Xenon4416 <xenonshrestha4416@gmail.com>
2019-12-25 08:02:08 +0545

    added fetch to signin

--- src/App.js
+++ src/App.js
@@ -4,29 +4,38 @@ import './App.css';
 
 class App extends Component {
     state = {
-        displayMode: 'SignUp',
-        signup: { email: null, password: null }
+        displayMode: 'SignIn',
+        signin: { email: null, password: null }
     };
 
     emailChangeHandler = (event) => {
-        let temp = {...this.state.signup};
+        let temp = {...this.state.signin};
         temp.email = event.target.value;
-        this.setState({signup: temp})
+        this.setState({signin: temp})
     };
 
     passwordChangeHandler = (event) => {
-        let temp = {...this.state.signup};
+        let temp = {...this.state.signin};
         temp.password = event.target.value;
-        this.setState({signup: temp})
+        this.setState({signin: temp})
     };
 
     onSignInHandler = () => {
-        //request here
+        fetch('http://localhost:3000/signin',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: this.state.signin.email,
+                password: this.state.signin.password
+            })
+        }).then()
     };
 
     render(){
         let mode = null;
-        if (this.state.displayMode === 'SignUp'){
+        if (this.state.displayMode === 'SignIn'){
             mode = <SignIn
                 email={this.emailChangeHandler}
                 password={this.passwordChangeHandler}
