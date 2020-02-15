3759b6c0d0aca0aea14ef4aa3d973c8c23fd08de
Xenon4416 <xenonshrestha4416@gmail.com>
2020-02-15 17:39:48 +0545

    added admin page handler

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -7,40 +7,61 @@ const Option = props => {
 };
 
 class Admin extends Component {
+    constructor (props){
+        super (props);
+        this.onAddClickHandler = this.onAddClickHandler.bind(this)
+        this.onSubmithandler = this.onSubmithandler.bind(this);
+    }
     state = {
         name:'',
         username:'',
         password:'',
-        classes:[{class:'074BCT',subjectCode:'SHA401'},{class:'074BCT',subjectCode:'SHA401'}],
+        classes:[],
         batch:['074BCT','074BEX'],
         subcodes:['SH401'],
         displaySelection: true
     };
 
-    onTextChangeHandler(event){
-        switch (event.target.name) {
-            case 'name':
-                this.setState({name: event.target.value});
-                break;
-            case 'username':
-                this.setState({username: event.target.value});
-                break;
-            case 'password':
-                this.setState({password: event.target.value});
-        }
+    onSubmithandler(){
+        let name = document.getElementById("name").value;
+        let username = document.getElementById('username').value;
+        let password = document.getElementById('password').value;
+        let reqBody = {name: name, username: username, password: password, classes: this.state.classes};
+        fetch('http://localhost:3000/signin',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(reqBody)
+        })
+            .then(res => res.json())
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
     }
 
-    onButtonClickHandler(event){
-        // switch (event.target.name) {
-        //     case 'add':
-        //         this.setState({name: event.target.value});
-        //         break;
-        //     case 'username':
-        //         this.setState({username: event.target.value});
-        //         break;
-        //     case 'password':
-        //         this.setState({password: event.target.value});
-        // }
+    onAddClickHandler(event){
+        switch (event.target.name) {
+            case 'add':
+                let sel_batch = document.getElementById("batch");
+                let sel_sub = document.getElementById('subcode');
+                let cls = {class:sel_batch.options[sel_batch.selectedIndex].text, subjectCode:sel_sub.options[sel_sub.selectedIndex].text};
+                let i = 0;
+                for (i=0;i<this.state.classes.length;i++){
+                    if (this.state.classes[i].class === cls.class && this.state.classes[i].subjectCode === cls.subjectCode){
+                        break
+                    }
+                }
+                if (i === this.state.classes.length || this.state.classes.length === 0){
+                    this.setState({classes: [...this.state.classes, cls]});
+                }
+                // this.setState({name: event.target.value});
+                break;
+            // case 'username':
+            //     this.setState({username: event.target.value});
+            //     break;
+            // case 'password':
+            //     this.setState({password: event.target.value});
+        }
     }
 
     toggleView(){ this.setState({displaySelection: !this.state.displaySelection}); }
@@ -52,35 +73,35 @@ class Admin extends Component {
                     <FormGroup row>
                         <Label sm={2}>Name</Label>
                         <Col sm={9}>
-                            <Input type="text" name="name" placeholder="Enter the Teacher name" />
+                            <Input type="text"  id="name" placeholder="Enter the Teacher name" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label sm={2}>Username</Label>
                         <Col sm={9}>
-                            <Input type="text" name="username" placeholder="Enter the Username" />
+                            <Input type="text" id="username" placeholder="Enter the Username" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label sm={2}>Password</Label>
                         <Col sm={9}>
-                            <Input type="password" name="password" placeholder="Enter the Password" />
+                            <Input type="password" id="password" placeholder="Enter the Password" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label sm={2}>Select Batch</Label>
-                        <Col sm={2}>
-                            <Input type="select" name="batch">
+                        <Col sm={3}>
+                            <Input type="select" name="batch" id='batch'>
                                 {this.state.batch.map((value) => <option>{value}</option>)}
                             </Input>
                         </Col>
                         <Col sm={2}>
-                            <Input type="select" name="subcode">
+                            <Input type="select" name="subcode" id='subcode'>
                                 {this.state.subcodes.map((value) => <option>{value}</option>)}
                             </Input>
                         </Col>
                         <Col sm={2}>
-                            <Button name='add'>Add</Button>
+                            <Button name='add' onClick={this.onAddClickHandler}>Add</Button>
                         </Col>
                         {/*<Col sm={3}>
                             <Button id="viewSelected">View Selected</Button>
@@ -92,6 +113,7 @@ class Admin extends Component {
                             </Popover>
                         </Col>*/}
                     </FormGroup>
+                    <Button onClick={this.onSubmithandler}>Submit</Button>
                 </Form>
             </div>
         );
