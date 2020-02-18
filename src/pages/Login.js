93efdda24b5cbe952b5ec2760577e5af20bb5da7
Bijay Shrestha <shresthabijay26@yahoo.com>
2020-02-18 06:59:33 +0545

    color

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -35,13 +35,14 @@ class Login extends React.Component{
         return(
             <div>
                 {authRedirect}
-            {errorMessage ? errorMessage : (<Form className="body" onSubmit={ (e) => this.submitForm(e) }>
+            {errorMessage ? errorMessage : (
+            <Form className="loginBody loginForm" onSubmit={ (e) => this.submitForm(e) }>
                     <img className="img" src="http://pcampus.edu.np/wp-content/uploads/2019/05/cropped-pcamus-header-image.jpg" alt="dfdsfdf"/>
                     <br/>
                     <h2 className="hd2">Internal Marks Management</h2>
                     <FormGroup className="text-white">
 
-                        <Label>Username</Label>
+                        <Label >Username</Label>
                         <Input
                             type="text"
                             id="username"
