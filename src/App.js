3aa0050d7b4b1cf285812886b40ba5b1c80dccd6
Bijay Shrestha <shresthabijay26@yahoo.com>
2020-02-20 13:26:30 +0545

    Combining

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component, Fragment } from 'react';
 import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
 import {Container,Row,Col} from 'reactstrap';
-import {StudentList} from "./pages/Teacher/StudentList";
+//import {StudentList} from "./pages/Teacher/StudentList";
 // import SignIn from './pages/SignIn';
 import LogIn from './pages/Login';
 import Admin from './pages/Admin';
-import Teacher from './pages/Teacher/Teacher';
-import Student from './pages/Student';
-import {SidebarUI} from "./components/Slidebar/SidebarUI";
-import {MarksEntry} from './pages/Teacher/MarksEntry';
+import {Navbars} from './NewUI/components/NavBar/Navbars'
+//import {NavBars} from './NewUI/components/NavBar/Navbars.js'
+import {StudentList} from './NewUI/pages/Teacher/StudentList';
+import {MarksEntry} from './NewUI/pages/Teacher/MarksEntry';
+//import Student from './pages/Student';
+import {SidebarUI} from "./NewUI/components/Slidebar/SidebarUI";
+//import {MarksEntry} from './pages/Teacher/MarksEntry';
 import * as actions from './store/actions/auth';
 import { connect } from 'react-redux';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -20,64 +23,51 @@ class App extends Component {
         this.props.onTryAutoSignup();
         console.log("in main page",this.props);
     }
+    
     render(){
-        let routes = (
-            <Switch>
-                <Route path="/" component={LogIn}/>
-            </Switch>
-        );
-
-        if(this.props.isAuthenticated && this.props.role==="admin"){
-            routes = (
-                <Container fluid={true}>
-               <Row>
-                   <Col>
-
-                   </Col>
-               </Row>
-               <Row>
-                   <Col xs={1}>
-                        <SidebarUI/>
-                   </Col>
-                   <Col xs={11}>
-                    <Switch>
-                        <Route path="/" component={Admin}/>
-                    </Switch>
-                   </Col>
-               </Row>
-           </Container>
-                
-            );
+        let routes =null;
+        let Switches=null;
+        if (!this.props.isAuthenticated){
+            routes=(
+                <Switch>
+                    <Route path="/" component={LogIn}/>
+                </Switch>)
         }
-        if(this.props.isAuthenticated && this.props.role==="teacher"){
-            routes = (
-                <Container fluid={true}>
-                <Row>
-                    <Col>
- 
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={1}>
-                         <SidebarUI/>
-                    </Col>
-                    <Col xs={11}>
-                     <Switch>
-                         <Route path="/" component={MarksEntry}/>
-                     </Switch>
-                    </Col>
-                </Row>
-            </Container>
+        else {
+            if(this.props.isAuthenticated && this.props.role==="admin"){
+            Switches = (
+                <Switch>
+                     <Route path="/" component={Admin}/>
+                 </Switch>
             );
-        }
-        // else if(this.props.isAuthenticated && this.props.role==="student"){
-        //     routes = (
-        //         <Switch>
-        //             <Route path="/" component={Student}/>
-        //         </Switch>
-        //     );
-        // }
+                }
+                else if(this.props.isAuthenticated && this.props.role==="teacher"){
+                    Switches = (
+                        <Switch>
+                            {/* <Route path="/" component={StudentListM}/> */}
+                            <Route path="/" component={MarksEntry}/>
+                        </Switch>
+                    );
+                }
 
+                routes=(
+                    <Container fluid={true}>
+                    <Row>
+                        <Col>
+                            <Navbars/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={1}>
+                            <SidebarUI/>
+                        </Col>
+                        <Col xs={11}>
+                        {Switches}
+                        </Col>
+                    </Row>
+                </Container>
+                )
+            }
         return (
             <Fragment>
                 { routes }
