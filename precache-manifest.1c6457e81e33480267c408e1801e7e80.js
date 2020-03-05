22d77aed74211b7e7412e6220dd02255de8f0102
Bijay Shrestha <shresthabijay26@yahoo.com>
2020-03-05 07:48:48 +0545

    Updates

---
+++ precache-manifest.1c6457e81e33480267c408e1801e7e80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18df29a3b07641caca4683f859e56ee3",
+    "url": "/se/index.html"
+  },
+  {
+    "revision": "dc448b5176149b8c66c4",
+    "url": "/se/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "6ecad9e2e50524f3616f",
+    "url": "/se/static/css/main.3cbc6c6c.chunk.css"
+  },
+  {
+    "revision": "dc448b5176149b8c66c4",
+    "url": "/se/static/js/2.2ce4e65e.chunk.js"
+  },
+  {
+    "revision": "6341dacbdbea9fb6de96ff28a6f7526e",
+    "url": "/se/static/js/2.2ce4e65e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ecad9e2e50524f3616f",
+    "url": "/se/static/js/main.488d0ccb.chunk.js"
+  },
+  {
+    "revision": "97bc3b557bc373ac7e35",
+    "url": "/se/static/js/runtime-main.ede9d338.js"
+  },
+  {
+    "revision": "af4df160b19b12275711506b69f9b1f9",
+    "url": "/se/static/media/tu logo official.af4df160.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b6f750b10fa95fd8c4d0f4d150d47928.js"
+  "/se/precache-manifest.1c6457e81e33480267c408e1801e7e80.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/se/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
