65f3d29db6ea83c3a6ff081104ffdf690d0b006d
Bijay Shrestha <32957118+zeroishero@users.noreply.github.com>
2020-09-13 14:30:39 +0545

    Update uris.js

--- src/NewUI/store/uris.js
+++ src/NewUI/store/uris.js
@@ -4,6 +4,6 @@ export const ASSIGN_TEACHER = baseURI+'/api/users/assignTeacher';
 export const ADD_TEACHER = baseURI+"/api/classes/addTeacher";
 export const FETCH_CLASS_LIST = baseURI+'/api/lecturers/';
 export const FETCH_CLASS_STUDENT_lIST = baseURI+'/api/marks/class/';
-export const POST_MARKS = baseURI+'/api/record/class/';
+export const POST_MARKS = baseURI+'/api/marks/class/';
 export const STUDENT_MARKS = baseURI+'/api/marks/';
 export const FETCH_TEACHER_LIST = baseURI+'/api/users?role=teacher';
